31ce15456f793b23e6f8a244ed1cc88da06a08be
RadyaHarbani <radya.i4m@gmail.com>
2023-07-21 21:35:44 +0700

    Latihan API Soccer

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'Model/PremierLeagueModel.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+PremierLeagueModel? premierLeaguaModel;
+bool isLoading = true;
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllList();
+  }
+
+  void getAllList() async {
+    setState(() {
+      isLoading = false;
+    });
+
+    final linkApi = await http.get(
+      Uri.parse(
+          'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League'),
+    );
+    premierLeaguaModel = PremierLeagueModel.fromJson(
+      json.decode(
+        linkApi.body.toString(),
+      ),
+    );
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Premier League',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu_rounded,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          body: isLoading == false
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: premierLeaguaModel!.teams!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Container(
+                        margin: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(200),
+                          color: Colors.white,
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                      premierLeaguaModel!
+                                          .teams![index].strTeamBadge!,
+                                    ),
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    premierLeaguaModel!.teams![index].strTeam!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Text(
+                                    premierLeaguaModel!
+                                        .teams![index].strAlternate!,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  Text(
+                                    premierLeaguaModel!
+                                        .teams![index].strStadium!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )),
+    );
+  }
+}
